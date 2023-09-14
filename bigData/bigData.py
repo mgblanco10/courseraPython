4de7724639734861e985a3ff3126c2e24b45cca3
mgblanco10 <mgblanco1001@gmail.com>
2023-09-14 14:56:42 +0200

    wip

--- bigData/bigData.py
+++ bigData/bigData.py
@@ -1,9 +1,7 @@
 import pandas as pd
 
-# Cargando los datos de nuestra big data de prueba
 data = pd.read_csv('datos.csv')  
 
-# Análisis básico de los datos 
 print("Número total de registros:", len(data))
 print("Columnas disponibles:", list(data.columns))
 
@@ -11,7 +9,7 @@ print("Columnas disponibles:", list(data.columns))
 # Supongamos que queremos encontrar el promedio de una columna 'ventas' en función de la columna 'mes'
 promedio_ventas_mes = data.groupby('mes')['ventas'].mean()
 
-# Visualización de resultados
+
 print("\nPromedio de ventas por mes:")
 print(promedio_ventas_mes)
 
